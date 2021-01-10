1224c6da7914b733d074993e2584cf998030b484
Ghaith Alsirawan <g.alsirawan@icloud.com>
2021-01-10 14:12:09 +0400

    cleaning .py file

--- Importing_web_table.py
+++ Importing_web_table.py
@@ -1,29 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Importing data to pandas
-
-# ### Importing via weblink (tables)
-
-# In[1]:
-
-
 import pandas as pd
-import ssl
 line = "============================================================================================"
 
 
-# In[13]:
-
-
-#verify my end to pull the data from the source 
-ssl._create_default_https_context = ssl._create_unverified_context
-
 #loading the wikipedia webpage into the dataframe
 data_tables = pd.read_html(input("Please write the full link of your data webpage source:\n"))
-
-
-# In[18]:
 
 
 #Extract the table in the webpage
@@ -39,9 +22,6 @@ print(line)
 the_table.head()
 
 
-# In[12]:
-
-
 #Save your datafarm table to CSV file
 try:
     the_table.to_csv(input(r"Please write the csv file name that want to save (filename.csv): "), index = False)
@@ -50,8 +30,7 @@ try:
 except:
     print("Something went wrong while saving the csv file!")
 
-
-# In[ ]:
+#End
 
 
 
